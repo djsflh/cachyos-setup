7333b46f5f9af8bfce3b6abaf5eea7beaa4161e6
djsflh <118718442+djsflh@users.noreply.github.com>
2026-04-08 17:00:13 -0500

    fix bug with spaces before [home]

--- installers/snap-pac-config.sh
+++ installers/snap-pac-config.sh
@@ -3,7 +3,7 @@ echo "--- Configuring snap-pac.ini ---"
 
 SNAPFILE="/etc/snap-pac.ini"
 
-if ! sudo grep -q "^\[home\]" "$SNAPFILE" 2>/dev/null; then
+if ! sudo grep -q "^\s*\[home\]" "$SNAPFILE" 2>/dev/null; then
     printf '\n[home]\n snapshot = True\n' | sudo tee -a "$SNAPFILE" > /dev/null
     echo "Added [home] config to $SNAPFILE"
 else
