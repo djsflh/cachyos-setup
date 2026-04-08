4f6883dd058dda5c019c46a7acec27100407c8bb
djsflh <118718442+djsflh@users.noreply.github.com>
2026-04-07 23:30:42 -0500

    Update btrfs-home.sh

--- installers/btrfs-home.sh
+++ installers/btrfs-home.sh
@@ -3,12 +3,10 @@
 # Check if home config already exists
     echo
 if sudo snapper list-configs | grep -q "^home "; then
-    echo
     echo "Snapper home config already exists, skipping."
     echo
 else
     # Create snapper config for /home
-    echo
     echo "Creating snapper config for /home"
     sudo snapper -c home create-config /home
     echo 
