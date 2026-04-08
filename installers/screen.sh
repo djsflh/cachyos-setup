657dfcc5cb6eab9c8b5b95373244913100a0054f
djsflh <118718442+djsflh@users.noreply.github.com>
2026-04-07 23:44:43 -0500

    Update screen.sh

--- installers/screen.sh
+++ installers/screen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo "--- Installing Screen ---"
 
-echo "  → Installing packages..." | tee -a "$LOGFILE"
+echo
 run_quiet "Installing screen packages" sudo pacman -Sy --noconfirm screen >> "$LOGFILE" 2>&1
 
 echo "Screen installed."
