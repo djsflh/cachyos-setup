2cc3f1e24a7f44a6f24af69241f5d9f75009958f
djsflh <118718442+djsflh@users.noreply.github.com>
2026-04-07 23:47:27 -0500

    Update screen.sh

--- installers/screen.sh
+++ installers/screen.sh
@@ -4,7 +4,7 @@ echo
 echo "Installing screen"
 
 echo
-echo "sudo pacman -Sy --noconfirm screen"
+echo "   sudo pacman -Sy --noconfirm screen"
 run_quiet "Installing screen" sudo pacman -Sy --noconfirm screen >> "$LOGFILE" 2>&1
 
 echo "Screen installed."
