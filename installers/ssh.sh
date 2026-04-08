4fc6a2584afb87bc38747d1c3356d5e8477c00dd
djsflh <118718442+djsflh@users.noreply.github.com>
2026-04-07 23:00:57 -0500

    revamp ssh.sh, add ufw rule

--- installers/ssh.sh
+++ installers/ssh.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-echo "--- Enabling SSH ---"
 
-echo "  → Installing packages..." | tee -a "$LOGFILE"
-run_quiet "Installing ssh packages" sudo pacman -Sy --noconfirm openssh >> "$LOGFILE" 2>&1
+echo
+echo "Installing openssh"
+echo "   sudo pacman -Sy --noconfirm openssh"
+run_quiet "Installing openssh" sudo pacman -Sy --noconfirm openssh >> "$LOGFILE" 2>&1
+echo
 
+echo "Enabling sshd"
+echo "   sudo systemctl enable sshd"
 sudo systemctl enable sshd
+echo
+
+echo "Starting sshd"
+echo "   sudo systemctl start sshd"
 sudo systemctl start sshd
+echo
+
+echo "Allowing 192.168.1.51 in firewall"
+echo "   sudo ufw allow from 192.168.1.51 to any port 22 proto tcp"
+sudo ufw allow from 192.168.1.51 to any port 22 proto tcp
+echo
 
 echo "SSH enabled and started."
